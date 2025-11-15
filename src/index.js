3544f3f39f272c2af22ca6245f5845985a2dd224
Aaron Jiane <devmfundo@gmail.com>
2025-11-15 18:48:21 +0200

    module bug try out fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const helmet = require("helmet");
 const cors = require("cors");
 const hpp = require("hpp");
 const { corsOptions } = require("./utils/configuration");
-const { getSymbolDataRoute } = require("./routes/market_stream_route");
+const { getSymbolDataRoute } = require("./routes/insert_symbol_price_route");
 const { uploadSymbolDataRoute } = require("./routes/symbol_price_route");
 const {
   unknownEndpoint,
--- src/routes/insert_symbol_price_route.js
+++ src/routes/insert_symbol_price_route.js
@@ -4,4 +4,4 @@ const { getSymbolData } = require("../controllers/get_symbol_price_controller");
 
 getSymbolDataRoute.post("/market/v1/symbol/query", getSymbolData);
 
-module.exports = {getSymbolDataRoute };
+module.exports = {getSymbolDataRoute};
