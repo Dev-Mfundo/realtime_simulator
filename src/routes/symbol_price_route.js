35b8232e0cc96b72d9ec233dc88a7378346cc1c1
Aaron Jiane <devmfundo@gmail.com>
2025-11-15 18:57:04 +0200

    get symbol data route path,

--- src/routes/symbol_price_route.js
+++ src/routes/symbol_price_route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const uploadSymbolDataRoute = express.Router();
-const { insertSymbolPrice } = require("../controllers/symbol_price_controller");
+const { insertSymbolPrice } = require("../controllers/get_symbol_price_controller");
 const { upload } = require("../utils/middleware");
 
 uploadSymbolDataRoute.post(
